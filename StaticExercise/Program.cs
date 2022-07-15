6989b67538af9399d9a1f4745cec1676758e4e58
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-15 13:58:39 -0500

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,9 @@
+﻿using StaticExercise;
+
+var celsius = TempConverter.FahrenheitToCelsius(212); 
+var fahrenheit = TempConverter.CelsiusToFahrenheit(100);
+
+
+Console.WriteLine($"FahrenheitToCelsius: {celsius}");
+
+Console.WriteLine($"CelsiusToFahrenheit: {fahrenheit}");
